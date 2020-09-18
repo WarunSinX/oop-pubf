23ba33b389ead8cb5b469c5b5a3150e3410d0fec
Amarin Kongthong <amarinkongthong@Amarins-MacBook-Pro.local>
2020-09-19 02:47:43 +0700

    Create Attrb MeTod BattleField

--- src/BattleField.java
+++ src/BattleField.java
@@ -1,5 +1,18 @@
 public class BattleField {
+    //attb
+    private int maxX;
+    private int maxY;
+
     public BattleField(int maxX, int maxY) {
         // Please implement this
+        this.maxX = maxX;
+        this.maxY = maxY;
+    }
+
+    public int getMaxX(){
+        return maxX;
+    }
+    public int getMaxY(){
+        return maxY;
     }
 }
