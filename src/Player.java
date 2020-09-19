7f60cdc25dbe2211cda2694cc47cdacddbe010a0
warunsinx <warunsinx@gmail.com>
2020-09-19 21:28:47 +0700

    Add func : setHealth

--- src/Player.java
+++ src/Player.java
@@ -61,6 +61,10 @@ public class Player {
         return yLoc;
     }
 
+    public void setHealth(int n) {
+        health = n;
+    }
+
     public int getAtkDmg() {
         return this.attackDamage;
     }
