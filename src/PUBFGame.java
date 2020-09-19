d7224f653f9aa48621b4ce5a9c261c808e935d52
Amarin Kongthong <amarinkongthong@Amarins-MacBook-Pro.local>
2020-09-19 19:22:07 +0700

    add phase 5 PUBFGAME

--- src/PUBFGame.java
+++ src/PUBFGame.java
@@ -2,12 +2,17 @@ public class PUBFGame {
     private Player[] players;
     private BattleField battleField;
 
-    public PUBFGame(BattleField f, Player[] p, int numPlayers) {
+    public PUBFGame(BattleField f, Player[] p) {
         // Please implement this
+        this.battleField = f;
+        this.players = p;
+    
     }
-
+    //maxNumRounds is another class for num the round.
+    //not finished will be up to date.
     public void play(int maxNumRounds) {
         // Please implement this
+       
     }
 
     public boolean playOneRound() {
@@ -20,6 +25,10 @@ public class PUBFGame {
 
     protected void phase1PlayersTakeAction() {
         // Please implement this
+    
+        //start check around in each player.
+        
+        //alive players can shoot , Dead can't
     }
 
     protected void phase2PlayersMove() {
@@ -53,5 +62,14 @@ public class PUBFGame {
 
     protected boolean phase5FindWinner() {
         // Please implement this
+        int countPL=0;
+        for(int i=0;i<players.length;i++){
+            if(players[i].isAlive()){
+                countPL++;
+            }
+        }
+        if(countPL==1||countPL==0)
+        return false;
+        else return true;
     }
 }
