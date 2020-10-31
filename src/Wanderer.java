f50d5c31ac641d41ee56105f58f43445e0a97b76
warunsinx <warunsinx@gmail.com>
2020-10-31 14:11:25 +0700

    Initialize Wanderer \need to implement move

---
+++ src/Wanderer.java
@@ -0,0 +1,10 @@
+public class Wanderer extends Player {
+    public Wanderer(int x, int y, String n) {
+        super(x, y, n);
+    }
+
+    @Override
+    public void move(int maxX, int maxY) {
+
+    }
+}
